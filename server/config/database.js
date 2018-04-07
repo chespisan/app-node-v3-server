30985d9b43f3b6e377b25c464cdcabdeae78aaac
Sergio Sanchez <sergiosan2802@gmail.com>
2018-04-07 01:46:45 -0500

    Version B.P 3 server

---
+++ server/config/database.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// modulo para la url de la base de datos
+module.exports = {
+    'url': 'mongodb://<username>:<password>@ds123456.mlab.com:49466/mongodbroles',
+    'urlLocal': 'mongodb://localhost:8080'
+}
